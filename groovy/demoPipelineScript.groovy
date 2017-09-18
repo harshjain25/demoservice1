328fd13ad168b7d7769ac7b0d9004a902d13c6b1
Harsh Jain <harsh@theinnovationinc.co>
2017-09-18 16:01:47 +0530

    demo service's pipeline groovy script updated to deploy till qa from checking out in dev

--- groovy/demoPipelineScript.groovy
+++ groovy/demoPipelineScript.groovy
@@ -1,10 +1,35 @@
 node{
 
-	stage('hello'){
-		sh 'echo helloworld'
+	stage('checkokut scm & package'){
+		git  url: gitHubUrl, 
+    	branch: branchName
+
+    	sh 'mvn clean package'
 	}
 
-	stage('done'){
-		sh 'echo done'
+	stage('unit-test'){
+		sh 'mvn test'
+	}
+
+	stage('integration-test'){
+		sh 'mvn integration-test -P integration'
+	}
+
+	stage('deploy-dev'){
+		sh 'echo "mvn spring-boot:run -Dspring.profiles.active=dev" | at now + 1 minutes'
 	}
 }
+
+node {
+	
+	// stage('checkokut scm & package'){
+	// 	git  url: gitHubUrl, 
+ //    	branch: branchName
+
+ //    	sh 'mvn clean package'
+	// }
+		
+	stage('deploy-qa'){
+		sh 'echo "mvn spring-boot:run -Dspring.profiles.active=qa" | at now + 1 minutes'
+	}		
+} 
